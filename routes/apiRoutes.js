133998c0e211acfd1ef30412d6ddbc3220b857f3
dd <1337176014@qq.com>
2023-06-29 00:22:25 +0800

    s

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,18 +3,11 @@ const apiController = require('../controllers/apiController');
 
 const router = express.Router();
 
-const { updatePayInfo, orderHandler } = require('../controllers/timingController')
-
 router.get('/price', apiController.getPrice);
-
 
 router.get('/users', function(req, res) {
   res.send('Hello from Node.js API');
 });
-
-router.get('/updatePayInfo', updatePayInfo)
-router.get('/orderHandler', orderHandler)
-
 
 
 router.get('*', (req, res) => {
