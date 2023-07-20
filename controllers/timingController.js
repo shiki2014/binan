dfccce0491f7a3a3384934530bb5efbcb8fa203c
shiki2014 <shiki2014@outlook.com>
2023-07-20 17:33:37 +0800

    恢复原状

--- controllers/timingController.js
+++ controllers/timingController.js
@@ -390,8 +390,7 @@ async function initData () {
 module.exports = async function () {
   global.logger.info('定时交易策略开始')
   // start()
-  // initData()
-  deleteAllInvalidOrders()
+  initData()
   schedule.scheduleJob('4 0 7,19 * * *',async function () {
     // 更新合约交易
     global.logger.info('更新合约对开始');
